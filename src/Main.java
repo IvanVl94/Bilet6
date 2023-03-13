1e2d7d1641aaf34fad95bba241b325d2bb7c99cc
IvanVl94 <WlasowIwan@yandex.ru>
2023-03-13 21:00:46 +0300

    Билет 6 Задача на проверку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public  static  void main (String[] args) {
+int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+massive(numbers);
+    }
+    public static  void massive ( int[]number){
+        for (int i = 0; i < number.length ; i++) {
+            System.out.println(number[i]);
+            if (number[i] % 2 == 0) {
+                number[i] +=1;
+            }
+
+        }
+        for (int i = 2; i < 7; i++) {
+            System.out.println(number[i]);
+
+        }
     }
 }

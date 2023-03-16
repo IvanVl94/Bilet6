3f58cfc7ab4cc9251bda4611372c5a238ac19193
IvanVl94 <WlasowIwan@yandex.ru>
2023-03-16 20:11:09 +0300

    Билет 6 Задача на проверку 2

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,17 @@ public class Main {
 int[] numbers = {1,2,3,4,5,6,7,8,9,10};
 massive(numbers);
     }
-    public static  void massive ( int[]number){
-        for (int i = 0; i < number.length ; i++) {
-            System.out.println(number[i]);
-            if (number[i] % 2 == 0) {
-                number[i] +=1;
-            }
+   public  static void massive (int[] number){
+       for (int i = 0; i < number.length; i++) {
+           System.out.println(number[i]);
+           if (number[i] % 2 ==0 ) {
+               number[i] += 1;
 
-        }
-        for (int i = 2; i < 7; i++) {
-            System.out.println(number[i]);
+           }
+       }
+       for (int i = 2; i < 7; i++) {
+           System.out.println(number[i]);
+       }
 
-        }
-    }
+       }
 }
